0f0b2b56e402698b69a789aa0d1a2053b44269fa
rudydudy21 <jonathan.rudy@gmail.com>
2026-08-21 10:14:44 -0400

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Feetsball 2026",
   description: "Weekly College Football Challenge",
+  icons: {
+    icon: "/icon.png",
+    apple: "/icon.png",
+  },
 };
 
 export default function RootLayout({
